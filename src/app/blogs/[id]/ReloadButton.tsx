d2528615e57a633fc7b6dd0efdff16faddb28e97
Groshan67 <ghassemroshan@gmail.com>
2026-06-17 15:00:04 +0330

    first

--- src/app/blogs/[id]/ReloadButton.tsx
+++ src/app/blogs/[id]/ReloadButton.tsx
@@ -23,7 +23,7 @@ export default function ReloadButton({ id }: { id: string }) {
                 >
                     <path d="M21.5 2v6h-6M2.5 22v-6h6M2 11.5a10 10 0 0 1 18.8-4.3M22 12.5a10 10 0 0 1-18.8 4.2" />
                 </svg>
-                再読み込み
+                Reload | 再読み込み
             </button>
         </form>
     );
